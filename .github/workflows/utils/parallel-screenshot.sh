a59b4e2dbaa54fab254a5cbdb38d3f21542b0783
yukinissie <bigtree2490@outlook.jp>
2023-02-16 23:51:21 +0900

    :construction_worker: ci(VRT): 速度改善

--- .github/workflows/utils/parallel-screenshot.sh
+++ .github/workflows/utils/parallel-screenshot.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 set -vx
 
-PARALLE=1
+PARALLE=2
 
 function run_storybook() {
   npm run storybook:ci >/dev/null 2>&1 &
-  # trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-}
-
-function run_screenshot() {
-  i=$1
-  npx storycap http://localhost:6006 --serverTimeout 600000 --shard ${i}/"$PARALLE" &
   echo $!
 }
 
-function wait_screenshot() {
-  pid=$1
-  wait $pid
+function run_screenshot() {
+  for i in `seq 1 $PARALLE`; do
+    npx storycap http://localhost:6006 --serverTimeout 600000 --shard ${i}/"$PARALLE" &
+  done
+
+  wait
 }
 
 function run() {
-  run_storybook
+  storybook_pid=`run_storybook`
 
-  for i in `seq 1 $PARALLE`; do
-    pid=$(run_screenshot ${i})
-    # wait_screenshot $pid
-  done
+  run_screenshot ${i}
+
+  kill -9 ${storybook_pid}
+
+  exit 0
 }
 
 function main() {
